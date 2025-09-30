d508792d80c36fb3515c8937c5d1ff013062df4e
Rafel Castaño Ribes <rcastan6@xtec.cat>
2025-09-30 11:02:51 +0200

    a

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ function configurarRefrescPeriodic(contenidor, intervalMs) {
       const divFiltres = contenidor.querySelector('.filtres');
       actualitzarDatalists(divFiltres, campsFiltrables, novesDades);
 
-      console.log('Dades i filtres actualitzats en segon pla:', contenidor.getAttribute("data-src"), "=", json.stringify(dadesPerContenidor.get(contenidor)));
+      console.log('Dades i filtres actualitzats en segon pla:', contenidor.getAttribute("data-src"), "=", JSON.stringify(dadesPerContenidor.get(contenidor)));
     } catch (err) {
       console.error('Error en la recàrrega periòdica:', err);
     }
